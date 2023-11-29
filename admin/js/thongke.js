e0cde0a335474db4b3a40c6477a6ac85a9d1f547
NguyenMinhThuan1704 <thuanpro9b@gmail.com>
2023-11-29 22:06:13 +0700

    update

--- admin/js/thongke.js
+++ admin/js/thongke.js
@@ -26,24 +26,7 @@ app.controller("HDNController", function ($scope, $http) {
     $scope.GetNPP();
 
     $scope.listHDN;
-    // $scope.page = 1;
-    // $scope.changePage = function (pageNum) {
-    //     $scope.page = pageNum;
-    //     $scope.GetHDN();
-    // };
-
-    // $scope.GetHDN = function () {
-    //     $http({
-    //         method: 'POST',
-    //         data: { page: $scope.page, pageSize: 10 },
-    //         url: current_url_ad + '/api/HoaDonNhap/searchHDN',
-    //     }).then(function (response) {
-    //         $scope.listHDN = response.data.data;
-    //     });
-    // };
-    // $scope.GetHDN();
-
-    document.getElementById('btnSearch').addEventListener('click', function() {
+    document.getElementById('btnSearch1').addEventListener('click', function() {
         $scope.listHDN;
         $scope.page = 1;
         $scope.changePage = function (pageNum) {
@@ -51,16 +34,16 @@ app.controller("HDNController", function ($scope, $http) {
             $scope.ThongKeHDN();
         };
 
-        $scope.calculateTotal = function () {
-            $scope.totalQuantity = 0;
-            $scope.totalAmount = 0;
-            $scope.totalItems = 0;
+        $scope.calculateTotal_HDN = function () {
+            $scope.totalQuantity_hdn = 0;
+            $scope.totalAmount_hdn = 0;
+            $scope.totalItems_hdn = 0;
 
         
             for (var i = 0; i < $scope.listHDN.length; i++) {
-                $scope.totalQuantity += parseInt($scope.listHDN[i].soLuong);
-                $scope.totalAmount += parseFloat($scope.listHDN[i].tongTien);
-                $scope.totalItems = $scope.listHDN.length;
+                $scope.totalQuantity_hdn += parseInt($scope.listHDN[i].soLuong);
+                $scope.totalAmount_hdn += parseFloat($scope.listHDN[i].tongTien);
+                $scope.totalItems_hdn = $scope.listHDN.length;
             }
         };
     
@@ -71,25 +54,93 @@ app.controller("HDNController", function ($scope, $http) {
             var maNPP = document.getElementById('TenNPP').value;
             maNhanVien = maNhanVien.trim() === "" ? 0 : maNhanVien;
             maNPP = maNPP.trim() === "" ? 0 : maNPP;
-            var fr_ngaytao = document.getElementById('search-export-fr').value;
-            var to_ngaytao = document.getElementById('search-export-to').value;
-            $http({
-                method: 'POST',
-                data: {
-                    page: $scope.page,
-                    pageSize: 10,
-                    ma_nv: maNhanVien,
-                    ma_npp: maNPP,
-                    fr_NgayTao: fr_ngaytao,
-                    to_NgayTao: to_ngaytao
-                },
-                url: current_url_ad + '/api/HoaDonNhap/thongke_hoadonnhap   ',
-            }).then(function (response) {
-                $scope.listHDN = response.data.data;
-                // $scope.totalItems = $scope.listHDN.length; 
-                $scope.calculateTotal();
-            });
+            var fr_ngaytao = document.getElementById('search-export-fr-hdn').value;
+            var to_ngaytao = document.getElementById('search-export-to-hdn').value;
+            if (fr_ngaytao === '' || to_ngaytao === '') {
+                alert('Vui lòng nhập thời gian cần thống kê.');
+            } else {
+                $http({
+                    method: 'POST',
+                    data: {
+                        page: $scope.page,
+                        pageSize: 10,
+                        ma_nv: maNhanVien,
+                        ma_npp: maNPP,
+                        fr_NgayTao: fr_ngaytao,
+                        to_NgayTao: to_ngaytao
+                    },
+                    url: current_url_ad + '/api/HoaDonNhap/thongke_hoadonnhap',
+                }).then(function (response) {
+                    $scope.listHDN = response.data.data;
+                    $scope.calculateTotal_HDN();
+                });
+            }
         };
         $scope.ThongKeHDN();
     });
+
+    document.getElementById('btnSearch2').addEventListener('click', function() {
+        $scope.listHDB;
+        $scope.page = 1;
+        $scope.changePage = function (pageNum) {
+            $scope.page = pageNum;
+            $scope.ThongKeHDB();
+        };
+
+        $scope.calculateTotal_HDB = function () {
+            $scope.totalQuantity_hdb = 0;
+            $scope.totalAmount_hdb = 0;
+            $scope.totalItems_hdb = 0;
+
+            for (var i = 0; i < $scope.listHDB.length; i++) {
+                $scope.totalQuantity_hdb += parseInt($scope.listHDB[i].soLuong);
+                $scope.totalAmount_hdb += parseFloat($scope.listHDB[i].tongGia);
+                $scope.totalItems_hdb = $scope.listHDB.length;
+            }
+        };
+    
+        $scope.ThongKeHDB = function () {
+            var fr_ngaytao = document.getElementById('search-export-fr-hdb').value;
+            var to_ngaytao = document.getElementById('search-export-to-hdb').value;
+            if (fr_ngaytao === '' || to_ngaytao === '') {
+                alert('Vui lòng nhập thời gian cần thống kê.');
+            } else {
+                $http({
+                    method: 'POST',
+                    data: {
+                        page: $scope.page,
+                        pageSize: 10,
+                        ten_khach: $scope.tenkh,
+                        fr_NgayTao: fr_ngaytao,
+                        to_NgayTao: to_ngaytao
+                    },
+                    url: current_url_ad + '/api/HoaDon/thongke',
+                }).then(function (response) {
+                    $scope.listHDB = response.data.data;
+                    $scope.calculateTotal_HDB();
+                });
+            }
+        };
+        $scope.ThongKeHDB();
+    });
 });
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Ẩn tất cả các nội dung tab
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Xóa hiệu ứng active cho tất cả các nút tab
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Hiển thị nội dung tab hiện tại và thêm hiệu ứng active cho nút tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
